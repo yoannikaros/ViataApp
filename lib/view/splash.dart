c7e7e4af36c2e7f72e60e98dade62584e0563cb5
YOAN <yoannikaros29@gmail.com>
2022-05-30 18:18:52 +0700

    Add Theme

---
+++ lib/view/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'theme.dart';
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), ()=> Navigator.pushNamed(context,'/sign-in')
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('aset/.png',)
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
